1d6d0406628b588e783fce081a806a57c67b1a7b
ALM <ALM@dhcp-v113-095.mobile.uci.edu>
2017-10-03 19:47:36 -0700

    Removed unused code, simplified is functions and moved regex test inline

--- variablestore.js
+++ variablestore.js
@@ -1,102 +1,86 @@
 "use strict";
 
-function validateRegex(reg) {
-   if(reg === undefined) return false;
-   return !(reg instanceof RegExp) ? false : true;
-}
-
-function toString(val) {
-   return val.toString() || (val + "");
-}
-
-function isString(str)    {
-   return typeof str === 'string';
-}
-
 function isObject(value) {
-  const type = typeof value;
-  return value != null && (type == 'object' && !(type === 'function'));
+  return (typeof value === 'object' && !(Array.isArray(value)));
 }
 
-class VariableStore {
-   constructor(regex = /^[a-zA-Z\_]\w*$/, restrictedNames = []) {
-      if(!validateRegex(regex))           throw new Error("Must pass regex to first parameter of VariableStore");
-      if(!Array.isArray(restrictedNames)) throw new Error("Must pass array to second parameter of VariableStore");
+function VariableStore(regex = /^[a-zA-Z\_]\w*$/, restrictedNames = []) {
+   if(!(regex instanceof RegExp))        throw new Error("Must pass regex to first parameter of VariableStore");
+   if(!Array.isArray(restrictedNames)) throw new Error("Must pass array to second parameter of VariableStore");
 
-      let variables = {};
+   let variables = {};
 
-      function validVariableName(varName) {
-         return (!regex.test(varName)) ? false : true;
+   function validVariableName(varName) {
+      return regex.test(varName) ? true : false;
+   }
+
+   function restrictedName(varName) {
+      return restrictedNames.indexOf(varName) !== -1;
+   }
+
+   this.has = function has(varName) {
+      return Reflect.has(variables, varName);
+   }
+
+   this.set = function set(obj) {
+      if(restrictedName(obj.name)) {
+         throw new Error(`Variable cannot be a reserved word: ${obj.name}`);
       }
+      _set(obj);
+   }
 
-      function restrictedName(varName) {
-         return restrictedNames.indexOf(varName) != -1;
-      }
+   this.setRestricted = function setRestricted(obj) {
+      _set(obj);
+   }
 
-      this.has = function has(varName) {
-         return Reflect.has(variables, varName);
+   function _set(obj) {
+      if(!(obj.name && obj.value)) {
+         throw new Error("Must provide name and value to set {name: Name, value: Value}");
       }
+      if(variables[obj.name] && variables[obj.name].const) {
+         throw new Error("Attempting to set constant variable");
+      }
+      if(!validVariableName(obj.name)) {
+         throw new Error(`Invalid variable name, must satisfy regex: ${regex}`);
+      }
+      variables[obj.name] = {value: obj.value, const: !!obj.const};
+   }
 
-      this.set = function set(obj) {
-         if(restrictedName(obj.name)) {
-            throw new Error(`Variable cannot be a reserved word: ${obj.name}`);
-         }
-         _set(obj);
+   this.get = function get(varName) {
+      if(!this.has(varName)) {
+         throw new Error("Variable does not exist");
       }
+      return variables[varName].value;
+   }
 
-      this.setRestricted = function setRestricted(obj) {
-         _set(obj);
+   this.del = function del(varName) {
+      if(Array.isArray(varName)) {
+         varName.map((elem) => delete variables[elem]);
+      } else {
+         delete variables[varName];
       }
+   }
 
-      function _set(obj) {
-         if(!(obj.name && obj.value)) {
-            throw new Error("Must provide name and value to set {name: Name, value: Value}");
-         }
-         if(variables[obj.name] && variables[obj.name].const) {
-            throw new Error("Attempting to set constant variable");
-         }
-         if(!validVariableName(obj.name)) {
-            throw new Error(`Invalid variable name, must satisfy regex: ${regex}`);
-         }
-         variables[obj.name] = {value: obj.value, const: !!obj.const};
-      }
+   this.clear = function clear() {
+      variables = {};
+   }
 
-      this.get = function get(varName) {
-         if(!this.has(varName)) {
-            throw new Error("Variable does not exist");
-         }
-         return variables[varName].value;
+   this.clearNonConst = function clearNonConst() {
+      let old = variables;
+      variables = {};
+      for(const prop in old) {
+          if(old[prop].const) {
+            variables[prop] = old[prop];
+          }
       }
+   }
 
-      this.del = function del(varName) {
-         if(Array.isArray(varName)) {
-            varName.map((elem) => delete variables[elem]);
-         } else {
-            delete variables[varName];
-         }
-      }
+   this.getVariableNames = function getVariableNames() {
+      return Object.keys(variables);
+   }
 
-      this.clear = function clear() {
-         variables = {};
-      }
-
-      this.clearNonConst = function clearNonConst() {
-         let old = variables;
-         variables = {};
-         for(const prop in old) {
-             if(old[prop].const) {
-               variables[prop] = old[prop];
-             }
-         }
-      }
-
-      this.getVariableNames = function getVariableNames() {
-         return Object.keys(variables);
-      }
-
-      this.getVariableObjects = function getVariableObjects() {
-         return variables;
-      }
+   this.getVariableObjects = function getVariableObjects() {
+      return variables;
    }
 }
 
